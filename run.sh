1a102e599462efd7c82b331ef27a7f6a5f320030
Hiroyuki Sano <sh19910711@gmail.com>
2016-03-23 14:37:33 +0900

    squid: add run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-exec squid3 -NYCd 1
+run() {
+  local cache_dir=/var/cache/package-cache
+  mkdir -p ${cache_dir}
+  chown -R proxy. ${cache_dir}
+  if [ ! -d ${cache_dir}/00 ]; then
+    squid3 -N -z
+  fi
+  exec squid3 -NYCd 1
+}
+
+run $@
